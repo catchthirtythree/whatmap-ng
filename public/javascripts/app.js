2eb3115a307e76777e87be7fd1b65966d5be63c5
Michael Heller <hellercmichael@gmail.com>
2015-11-27 11:33:41 -0500

    General cleanliness. Responses with no items now link the user to youtube and don't crash.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,6 @@
 	
 	'use strict';
 	
-	var channel = "https://www.googleapis.com/youtube/v3/search?&forUsername=ksfrecords&q={0}&key=AIzaSyBAiqL_S3tVfpHNHix6sJ9vlcbIcw3X1VQ%20&part=snippet";
-	
 	var app = angular.module('app', [ 'ngRoute', 'ngSanitize' ]).
 	
 		config(function($locationProvider, $routeProvider) {
@@ -34,10 +32,6 @@
 		this.query = '';
 		
 		this.submit = function() {
-			// Log the query in the console.
-			console.log("Query:", this.query);
-			
-			// Send the user to the search controller.
 			$location.path('/search/' + this.query);
 		}
 	});
@@ -45,91 +39,78 @@
 	app.controller("MapController", function($http, $routeParams, $sce, $scope) {
 		$scope.map = {};
 		
-		$http.get('../api/maps/' + $routeParams.id).
+
+		this.init = function() {
+			// Query map api.
+			$http.get('../api/maps/' + $routeParams.id).
 		
-			success(function(data) {
-				// Log data in console.
-				console.log("Map Data:", data);
-				
-				if (data.success) {
-					var map = data.map;
-				
-					// Search youtube for a video of the map.
-					$http.get(channel.format(map.name)).
-					
-						success(function(response) {
-							// Log youtube response.
-							console.log("Response:", response);
-							
-							var video;
-							if (response.items) {
-								video = "<iframe width=\"560\" height=\"315\" src=\"http://www.youtube.com/embed/" + response.items[0].id.videoId + "\" frameborder=\"0\" allowfullscreen></iframe>";
-							} else {
-								video = "No video was found, try going <a href=\"http://www.youtube.com/results?search_query=" + map.name.replace(/_/g, " ") + " wr\" target=\"youtubes\">here</a>.";
-							}
-							
-							// Attach video to scope.
-							map.video = $sce.trustAsHtml(video);
+				success(function(data) {
+					if (data.success) {
+						var map = data.map;
+		
+						var channel = "https://www.googleapis.com/youtube/v3/search?&forUsername=ksfrecords&q={0}&key=AIzaSyBAiqL_S3tVfpHNHix6sJ9vlcbIcw3X1VQ%20&part=snippet";
+						// Search youtube for a video of the map.
+						$http.get(channel.format(map.name.replace(/_/g, " ") + ' wr')).
 						
-							// Push map to scope.
-							$scope.map = map;
-						}).
-						
-						error(function(err) {
-							// Log error in console.
-							console.log("Error:", err);
+							success(function(response) {
+								// Log youtube response.
+								console.log("Response:", response);
+								
+								var video;
+								if (response.items.length > 0) {
+									video = "<iframe width=\"560\" height=\"315\" src=\"http://www.youtube.com/embed/" + response.items[0].id.videoId + "\" frameborder=\"0\" allowfullscreen></iframe>";
+								} else {
+									video = "<h4 style=\"text-align:center\">No video was found, try going <a href=\"http://www.youtube.com/results?search_query=" + map.name.replace(/_/g, " ") + " wr\" target=\"youtubes\">here</a>.</h4>";
+								}
+								
+								map.video = $sce.trustAsHtml(video);
+								$scope.map = map;
+							}).
 							
-							// Push error to scope.
-							$scope.error = err;
-						});
-				} else {
-					console.log("Error data:", data);
-					$scope.error = data;
-				}
-			}).
-			
-			error(function(err) {
-				// Log error in console.
-				console.log("Error", err);
+							error(function(err) {
+								$scope.error = err;
+							});
+					} else {
+						$scope.error = data;
+					}
+				}).
 				
-				// Push error to scope.
-				$scope.error = err;
-			});
+				error(function(err) {
+					$scope.error = err;
+				});
+		}
+		
+		this.init();
 	});
 	
 	app.controller("SearchController", function($http, $location, $routeParams, $scope) {
 		$scope.results = [];
 		
-		$http.get('../api/search/' + $routeParams.query).
+		this.init = function() {
+			// Query search api.
+			$http.get('../api/search/' + $routeParams.query).
 		
-			success(function(data) {
-				// Log data in console.
-				console.log("Search Data:", data);
-				
-				// Check length of data.maps and send to maps if needed.
-				if (data.success) {
-					if (data.map) {
-						$location.path('/maps/' + data.map.id);
+				success(function(data) {
+					if (data.success) {
+						if (data.map) {
+							$location.path('/maps/' + data.map.id);
+						} else {
+							data.maps.forEach(function(map) {
+								$scope.results.push(map);
+							});
+						}
 					} else {
-						// Push maps to scope.
-						data.maps.forEach(function(map) {
-							console.log("Map:", map);
-							$scope.results.push(map);
-						});
+						console.log("Error data:", data);
+						$scope.error = data;
 					}
-				} else {
-					console.log("Error data:", data);
-					$scope.error = data;
-				}
-			}).
-			
-			error(function(err) {
-				// Log error in console.
-				console.log("Error", err);
+				}).
 				
-				// Push error to scope.
-				$scope.error = err;
-			});
+				error(function(err) {
+					$scope.error = err;
+				});
+		}
+		
+		this.init();
 	});
 	
 	if (!String.prototype.format) {
